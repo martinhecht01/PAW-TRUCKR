203a34fed2055666e75ac7a9a6048112224d8ca2
Martin Hexht <mhecht@itba.edu.ar>
2023-04-03 20:33:37 -0300

    trip controller bug fix

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TripController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TripController.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class TripController {
 
     @RequestMapping("/trip")
