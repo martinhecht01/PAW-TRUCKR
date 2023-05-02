a7b06a90ad62aac24ba9efcc1b772bc15c7ef7cd
TobiasPerry <tperry@itba.edu.ar>
2023-05-02 17:59:07 -0300

    feat: mailing complete

--- interfacesServices/src/main/java/ar/edu/itba/paw/interfacesServices/MailService.java
+++ interfacesServices/src/main/java/ar/edu/itba/paw/interfacesServices/MailService.java
@@ -1,19 +1,18 @@
 package ar.edu.itba.paw.interfacesServices;
 
-import ar.edu.itba.paw.models.Proposal;
-import ar.edu.itba.paw.models.Trip;
-import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.*;
+
 import javax.mail.MessagingException;
 
 
 public interface MailService {
 
-    public void sendEmail(String toEmailAddress);
-
-    public void sendEmailTrip(User trucker, User accepted, Trip trip);
     public void sendProposalEmail(User user, Proposal proposal) throws MessagingException;
     public void sendTripEmail(User user,Trip trip) throws MessagingException;
     void sendConfirmationEmail(User user) throws MessagingException;
     public void sendResetEmail(User user,Integer hash) throws MessagingException;
+    void sendProposalRequestEmail(User user, ProposalRequest proposal) throws MessagingException;
+
+    public void sendRequestEmail(User user, Request request) throws MessagingException;
 
 }
--- services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
@@ -1,9 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfacesServices.MailService;
-import ar.edu.itba.paw.models.Proposal;
-import ar.edu.itba.paw.models.Trip;
-import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.mail.SimpleMailMessage;
@@ -26,12 +24,6 @@ public class MailServiceImpl implements MailService {
     @Autowired
     private TemplateEngine templateEngine;
 
-    private String generateEmailContent() {
-        Context context = new Context();
-        context.setVariable("title", "My Email Title");
-        context.setVariable("message", "Hello, this is my email message.");
-        return templateEngine.process("emailTemplate.html", context);
-    }
 
     private String generateEmailConfirmation(User confirmed) {
         Context context = new Context();
@@ -64,6 +56,22 @@ public class MailServiceImpl implements MailService {
         helper.setText(htmlContent, true);
         mailSender.send(message);
     }
+    private String generateRequestConfirmation(User user, Request confirmed) {
+        Context context = new Context();
+        context.setVariable("user", user);
+        context.setVariable("request", confirmed);
+        return templateEngine.process("requestconfirmation.html", context);
+    }
+
+    public void sendRequestEmail(User user,Request request) throws MessagingException {
+        String htmlContent = generateRequestConfirmation(user,request);
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(user.getEmail());
+        helper.setSubject("Request confirmation");
+        helper.setText(htmlContent, true);
+        mailSender.send(message);
+    }
     private String generateProposal(User user, Proposal proposal) {
         Context context = new Context();
         context.setVariable("user", user);
@@ -80,6 +88,23 @@ public class MailServiceImpl implements MailService {
         helper.setText(htmlContent, true);
         mailSender.send(message);
     }
+    private String generateProposalRequest(User user, ProposalRequest proposal) {
+        Context context = new Context();
+        context.setVariable("user", user);
+        context.setVariable("proposal", proposal);
+        return templateEngine.process("proposal.html", context);
+    }
+
+    public void sendProposalRequestEmail(User user, ProposalRequest proposal) throws MessagingException {
+        String htmlContent = generateProposalRequest(user,proposal);
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(user.getEmail());
+        helper.setSubject("Trip Proposal!");
+        helper.setText(htmlContent, true);
+        mailSender.send(message);
+    }
+
     private String generateReset(User user, Integer hash) {
         Context context = new Context();
         context.setVariable("user", user);
@@ -95,56 +120,6 @@ public class MailServiceImpl implements MailService {
         helper.setSubject("Password Reset");
         helper.setText(htmlContent, true);
         mailSender.send(message);
-    }
-
-
-
-
-    @Override
-    public void sendEmail(String toEmailAddress){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("tomigayba02yt@gmail.com");
-        message.setTo(toEmailAddress);
-        message.setText("Hola! Te has registrado en CoTruck!");
-        message.setSubject("Ya estas registrado en CoTruck.");
-
-        mailSender.send(message);
-    }
-
-    public void sendEmailTrip(User trucker, User accepted, Trip trip){
-        SimpleMailMessage message1 = new SimpleMailMessage();
-        message1.setFrom("tomigayba02yt@gmail.com");
-        message1.setTo(trucker.getEmail());
-        message1.setText("El viaje fue aceptado por: "+ accepted.getName()+ "\n Datos del viaje" +
-                "\nPatente:  "+ trip.getLicensePlate()+"\nOrigen: "+ trip.getOrigin()+"  -  "+trip.getDepartureDate()
-                + "\nDestino:" +trip.getDestination() +"  -  " + trip.getArrivalDate() + "\n");
-        message1.setSubject("Tu viaje ha sido aceptado");
-
-        mailSender.send(message1);
-
-        SimpleMailMessage message2 = new SimpleMailMessage();
-        message2.setFrom("tomigayba02yt@gmail.com");
-        message2.setTo(accepted.getEmail());
-        message2.setText("El viaje ha sido confirmado por:  "+ trucker.getName()+"\n Datos del viaje" +
-                "\nPatente:  "+ trip.getLicensePlate()+"\nOrigen: "+ trip.getOrigin()+"  -  "+trip.getDepartureDate()
-                + "\nDestino:" +trip.getDestination() +"  -  " + trip.getArrivalDate() + "\n" +
-                "Precio a cancelar:  " + trip.getPrice());
-
-
-        message2.setSubject("Viaje confirmado");
-//        String email,
-//        String name,
-//        String id,
-//        String licensePlate,
-//        int availableWeight,
-//        int availableVolume,
-//        LocalDateTime departureDate,
-//        LocalDateTime arrivalDate,
-//        String origin,
-//        String destination,
-//        String type
-
-        mailSender.send(message2);
     }
 
 
