35ef47517f534a0bd72316fb43a9bac1348e633a
Martin Hexht <mhecht@itba.edu.ar>
2023-05-11 19:24:23 -0300

    refactor: added overrides and cleaned up interface

--- interfacesServices/src/main/java/ar/edu/itba/paw/interfacesServices/MailService.java
+++ interfacesServices/src/main/java/ar/edu/itba/paw/interfacesServices/MailService.java
@@ -7,13 +7,11 @@ import javax.mail.MessagingException;
 
 public interface MailService {
 
-    public void sendProposalEmail(User user, Proposal proposal) throws MessagingException;
-    public void sendTripEmail(User user,User user2,Trip trip) throws MessagingException;
+    void sendProposalEmail(User user, Proposal proposal) throws MessagingException;
+    void sendTripEmail(User user,User user2,Trip trip) throws MessagingException;
     void sendConfirmationEmail(User user) throws MessagingException;
-    public void sendResetEmail(User user,Integer hash) throws MessagingException;
+    void sendResetEmail(User user,Integer hash) throws MessagingException;
     void sendProposalRequestEmail(User user, Proposal proposal) throws MessagingException;
-
-    public void sendRequestEmail(User user,User user2, Trip request) throws MessagingException;
-
+    void sendRequestEmail(User user,User user2, Trip request) throws MessagingException;
     void sendSecureTokenEmail(User user, Integer tokenValue) throws MessagingException;
 }
