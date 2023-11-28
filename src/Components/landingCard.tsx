cb4214dfee2e2e5610774e03aae302941bad6c17
tgaybare <tomigayba02yt@gmail.com>
2023-11-27 21:19:29 -0300

    ft: landing page done

---
+++ src/Components/landingCard.tsx
@@ -0,0 +1,17 @@
+import {Card} from "antd";
+import React from "react";
+import { Typography } from 'antd';
+const {Title} = Typography;
+
+const landingCard: React.FC<{ title: string; subtitle: string }> = ({ title, subtitle }) => {
+    return (
+        <Card style={{ textAlign: 'center', width: '80%', marginBottom:'1vh' }}>
+            <Title style={{ marginTop: '0vh' }} level={2}>
+                <b>{title}</b>
+            </Title>
+            <Title level={5}>{subtitle}</Title>
+        </Card>
+    );
+};
+
+export default landingCard;
