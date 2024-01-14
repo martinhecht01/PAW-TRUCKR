2c33a2aae3d0e1cdb6bd51101af60bc700788c93
MANUEL ESTEBAN DITHURBIDE <mdithurbide@itba.edu.ar>
2024-01-14 13:45:18 -0300

    refactor: profile

--- Front/src/pages/profile.tsx
+++ Front/src/pages/profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Avatar, Button, Card, Typography} from 'antd';
+import {Avatar, Button, Card, Col, Row, Typography} from 'antd';
 import '../styles/main.scss';
 import '../styles/profile.scss';
 import {useTranslation} from "react-i18next";
@@ -19,43 +19,54 @@ const profile: React.FC = () => {
 
     return (
         <div>
-        <div className="flex-center">
-            <Card style={{width:'30%',margin:'3vh'}} headStyle={{fontSize:'3vh', color:'#142d4c'}} title={t("profile.profile")}>
-                <div className='flex-center'>
-                    <Avatar size={124} icon={<UserOutlined />} />
-                </div>
-                <Title level={5}>{t("profile.name")}</Title>
-                <Text>dfkjdf</Text>
-                <Title level={5}>{t('profile.cuit')}</Title>
-                <Text>dfkjdf</Text>
-                <Title level={5}>{t('profile.email')}</Title>
-                <Text>dfkjdf</Text>
-                <Button style={{width:'100%', marginTop:'5vh'}} type='primary'>{t("profile.editProfile")}</Button>
-            </Card>
-            <Card headStyle={{fontSize:'3vh', color:'#142d4c'}} title={t("profile.completedTrips")}>
-                <Text>0</Text>
-            </Card>
-        </div>
-        <div className='flex-center'>
-            <Card headStyle={{fontSize:'2.5vh', color:'#142d4c'}} style={{width:'60%'}} title={
-                <div style={{justifyContent:'center',alignItems:'center', justifyItems:'center'}}>
-                    <StarFilled></StarFilled>
-                    {cardTitle}
-                </div>
-            }>
-                <div className='reviewsContainerStyle' ref={setContainer}>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                    <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
-                </div>
-            </Card>
-        </div>
+            <div className="space-evenly">
+                <Row className='w-80 space-evenly' style={{alignItems: 'start'}}>
+                    <Col span={12}>
+                        <Card className='w-100' title={<Title level={3}>{t("profile.profile")}</Title>}>
+                            <div className='flex-center'>
+                                <Avatar size={124} icon={<UserOutlined />} />
+                            </div>
+                            <Title level={5}>{t("profile.name")}</Title>
+                            <Text>dfkjdf</Text>
+                            <Title level={5}>{t('profile.cuit')}</Title>
+                            <Text>dfkjdf</Text>
+                            <Title level={5}>{t('profile.email')}</Title>
+                            <Text>dfkjdf</Text>
+                            <Button style={{width:'100%', marginTop:'5vh'}} type='primary'>{t("profile.editProfile")}</Button>
+                        </Card>
+                    </Col>
+                    <Col span={7}>
+                        <Card title={<Title level={3}>{t("profile.completedTrips")}</Title>}>
+                            <div className='w-100 text-center'> 
+                                <Title level={3}>0</Title>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+            <div className='space-evenly'>
+                <Row className='w-80 space-evenly mt-5' style={{alignItems: 'start'}}>
+                    <Col span={20}>
+                        <Card
+                            title={
+                                <Title level={3}><StarFilled></StarFilled>{cardTitle}</Title>
+                            }
+                        >
+                            <div className='reviewsContainerStyle' ref={setContainer}>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                                <ReviewContainer avgRating={5} comment={'Muy Bueno'}></ReviewContainer>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 };
