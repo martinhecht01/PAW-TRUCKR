21bcfbcfa1780f6bafdfce39fdbb59451a5277e2
MANUEL ESTEBAN DITHURBIDE <mdithurbide@itba.edu.ar>
2024-02-03 15:35:28 -0300

    feat: router roles + isAuth routes

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -26,6 +26,7 @@ import CreateAlert from "./pages/createAlert";
 import SentOffers from "./pages/sentOffers";
 import Tester from './pages/endpointTester.tsx';
 import AuthProvider from './hooks/authProvider.tsx';
+import { CustomRoute } from './Components/PrivateRoute.tsx';
 
 
 const WebApp = () => {
@@ -41,7 +42,6 @@ const WebApp = () => {
   return(
   <ConfigProvider
     theme={{
-      
         token: {
           "colorPrimary": "#142d4c",
           "colorInfo": "#142d4c",
@@ -53,28 +53,36 @@ const WebApp = () => {
       <AuthProvider>
         <CustomLayout>
           <Routes>
-            <Route path="/" element={<Landing/>}/>
-            <Route path="/trips" element={<BrowseTrips tripOrRequest='TRIP'/>}/>
-            <Route path="/cargo" element={<BrowseTrips tripOrRequest='REQUEST'/>}/>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="/myItinerary" element={<MyItinerary/>} />
-            <Route path="/myAlert" element={<MyAlert/>} />
-            <Route path="/profile/:userId" element={<Profile/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/profile/edit" element={<EditProfile/>} />
-            <Route path="/pastTrips" element={<PastTrips/>}></Route>
-            <Route path="/sentOffers" element={<SentOffers/>}></Route>
-            <Route path="/myPublications" element={<MyPublications/>}></Route>
-            <Route path="/trips/create" element={<CreateTrip/>}></Route>
-            <Route path="/trips/manage/:tripId" element={<ManageTrip/>}></Route>
-            <Route path="/createAlert" element={<CreateAlert/>}></Route>
-            <Route path="/trips/:tripId" element={<PublicationDetails/>}></Route>
-            <Route path="/searchTrips" element={<SearchTrips/>}></Route>
-            <Route path="/resetPassword" element={<ResetPassword/>}></Route>
-            <Route path="/resetPasswordRequest" element={<ResetPasswordRequest/>}></Route>
-            <Route path="/sendCounterOffer" element={<SendCounterOffer/>}></Route>
+
             <Route path='/test' element={<Tester/>}/>
+            
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/trips" element={<CustomRoute render={() => <BrowseTrips tripOrRequest='TRIP'/>} noAuth possibleRoles={['PROVIDER']}></CustomRoute>}/>
+            <Route path="/cargo" element={<CustomRoute render={() => <BrowseTrips tripOrRequest='REQUEST'/>} noAuth possibleRoles={['TRUCKER']}></CustomRoute>}/>
+            <Route path="/login" element={<CustomRoute render={() => <Login/>} noAuth/>} />
+            <Route path="/register" element={<CustomRoute render={() => <Register/>} noAuth/>} />
+            <Route path="/myItinerary" element={<CustomRoute render={() => <MyItinerary/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>} />
+            <Route path="/myAlert" element={<CustomRoute render={() => <MyAlert/>} possibleRoles={['TRUCKER']}/>} />
+            <Route path="/profile/:userId" element={<Profile/>} />
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/profile/edit" element={<CustomRoute render={() => <EditProfile/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}/>
+            
+            
+            <Route path="/pastTrips" element={<CustomRoute render={() => <PastTrips/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            <Route path="/sentOffers" element={<CustomRoute render={() => <SentOffers/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            <Route path="/myPublications" element={<CustomRoute render={() => <MyPublications/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            <Route path="/trips/create" element={<CustomRoute render={() => <CreateTrip/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            <Route path="/trips/manage/:tripId" element={<CustomRoute render={() => <ManageTrip/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            <Route path="/createAlert" element={<CustomRoute render={() => <CreateAlert/>} possibleRoles={['TRUCKER']}/>}></Route>
+
+            <Route path="/trips/:tripId" element={<PublicationDetails/>}></Route>
+            
+            <Route path="/searchTrips"element={<CustomRoute render={() => <SearchTrips/>} possibleRoles={['PROVIDER']}/>}></Route>
+            
+            <Route path="/resetPassword" element={<CustomRoute render={() => <ResetPassword/>} noAuth/>}></Route>
+            <Route path="/resetPasswordRequest" element={<CustomRoute render={() => <ResetPasswordRequest/>} noAuth/>}></Route>
+            <Route path="/sendCounterOffer" element={<CustomRoute render={() => <SendCounterOffer/>} possibleRoles={['TRUCKER', 'PROVIDER']}/>}></Route>
+            
             <Route path="*" element={<NotFound404/>} />
             <Route path="/404" element={<NotFound404/>} />
           </Routes>
