e65042198263d1579d74f53fbc22d5f6b6394c2d
Martin Hexht <mhecht@itba.edu.ar>
2024-02-03 20:48:03 -0300

    Fix: exception

--- webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/AccessDeniedExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/AccessDeniedExceptionMapper.java
@@ -1,31 +1,31 @@
-//package ar.edu.itba.paw.webapp.mapper;
-//import ar.edu.itba.paw.webapp.dto.ErrorDto;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.MessageSource;
-//import org.springframework.context.i18n.LocaleContextHolder;
-//import org.springframework.security.access.AccessDeniedException;
-//import org.springframework.stereotype.Component;
-//
-//import javax.ws.rs.core.GenericEntity;
-//import javax.ws.rs.core.Response;
-//import javax.ws.rs.ext.ExceptionMapper;
-//import javax.ws.rs.ext.Provider;
-//
-//@Component
-//@Provider
-//public class AccessDeniedExceptionMapper implements ExceptionMapper<AccessDeniedException> {
-//
-//    @Autowired
-//    private MessageSource messageSource;
-//
-//    @Override
-//    public Response toResponse(AccessDeniedException exception) {
-//        System.out.println("hola1");
-//        String message = messageSource.getMessage("exception.AccessDenied", null, LocaleContextHolder.getLocale());
-//        ErrorDto errorDto = ErrorDto.fromErrorMessage(message);
-//        return Response
-//                .status(Response.Status.FORBIDDEN)
-//                .entity(new GenericEntity<ErrorDto>(errorDto) {})
-//                .build();
-//    }
-//}
+package ar.edu.itba.paw.webapp.mapper;
+import ar.edu.itba.paw.webapp.dto.ErrorDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Component
+@Provider
+public class AccessDeniedExceptionMapper implements ExceptionMapper<ForbiddenException> {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    public Response toResponse(ForbiddenException exception) {
+        String message = messageSource.getMessage("exception.Forbidden", null, LocaleContextHolder.getLocale());
+        ErrorDto errorDto = ErrorDto.fromErrorMessage(message);
+        return Response
+                .status(Response.Status.FORBIDDEN)
+                .entity(new GenericEntity<ErrorDto>(errorDto) {})
+                .build();
+    }
+}
