37d57320507d93ccffc178c2ff80661263a43d37
tgaybare <tomigayba02yt@gmail.com>
2024-02-04 14:15:42 -0300

    ft: login tests

--- Front/src/test/pages/login.test.tsx
+++ Front/src/test/pages/login.test.tsx
@@ -4,20 +4,21 @@ import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom'
 import Login from "../../pages/login.tsx";
 import {ConfigProvider} from "antd";
+import {useTranslation} from "react-i18next";
 
+// const {t} = useTranslation()
 
 const customRender = (ui: React.ReactElement, options = {}) => render(ui, {
     wrapper: ({ children }) => <ConfigProvider prefixCls="bingo">{children}</ConfigProvider>,
     ...options,
 });
 
-
 afterEach(() => {
     localStorage.clear();
     // mockLogin.mockClear();
 })
 
-const mockedUsedNavigate = vi.fn();
+const mockedUseNavigate = vi.fn();
 
 // const mockedAuthLogin = vi.spyOn(useAuthContext(), 'login');
 // mockedAuthLogin
@@ -30,19 +31,19 @@ afterEach(() => {
 })
 
 vi.mock('react-router-dom', () => ({
-    useNavigate: () => mockedUsedNavigate,
-}));
-
-vi.mock('../../api/userApi.tsx', () => ({
-    loginUser: () => {localStorage.setItem('token','token');return new Promise((resolve) => {
-        setTimeout(() => {
-            const authToken = "token"; // Your string value
-            resolve(authToken);
-        }, 1000); // Adjust the timeout as needed
-    });}
+    useNavigate: () => mockedUseNavigate,
 }));
 
 test('Login page', async () => {
+
+    vi.mock('../../api/userApi.tsx', () => ({
+        loginUser: () => {localStorage.setItem('token','token');return new Promise((resolve) => {
+            setTimeout(() => {
+                const authToken = "token";
+                resolve(authToken);
+            }, 1000);
+        });}
+    }));
 
     customRender(<Login/>);
 
@@ -55,8 +56,26 @@ test('Login page', async () => {
 
     await userEvent.click(screen.getByTestId('button-login'));
 
-    // expect(screen.getByTestId('cuit-login')).toBeInTheDocument()
     expect(localStorage.getItem('token')).not.toBeNull();
-    // expect(mockedUsedNavigate).toHaveBeenCalledWith('/profile');
-    //TODO : fix and expand this test
-})
+    // expect(mockedUseNavigate).toHaveBeenCalledWith('/profile'); //TODO: ver si puedo hacer esto
+});
+
+test('Login page with invalid credentials', async () => {
+
+    vi.mock('../../api/userApi.tsx', () => ({
+        loginUser: () => {return new Promise((resolve) => {resolve(null)})}}));
+
+    customRender(<Login/>);
+
+    fireEvent.change(screen.getByTestId('cuit-login'),
+    { target: { value: '20-43724688-3' }
+    });
+    fireEvent.change(screen.getByTestId('password-login'),
+    { target: { value: 'password' }
+    });
+
+    await userEvent.click(screen.getByTestId('button-login'));
+
+    expect(localStorage.getItem('token')).toBeNull();
+    // expect(screen.getByText(t())).toBeInTheDocument(); //TODO : add what needs to be read
+});
